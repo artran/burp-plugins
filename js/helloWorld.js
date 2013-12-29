84b1048f89a2f5998865a28336fa39bd9a9609c1
Ray Tran <ray@artran.co.uk>
2013-12-29 14:44:51 +0000

    Added an JavaScript extension to show basic functionality.
    Based on the Python HellopWorld.py extension, which I've also added in
    this commit.

---
+++ js/helloWorld.js
@@ -0,0 +1,20 @@
+importClass(java.io.PrintWriter);
+importClass(java.lang.RuntimeException);
+
+var registerExtenderCallbacks = function(callbacks) {
+    callbacks.setExtensionName("Hello world extension");
+
+    // Set up output and error streams
+    var stdout = PrintWriter(callbacks.getStdout(), true),
+        stderr = PrintWriter(callbacks.getStderr(), true);
+
+    // Print messages to the output and error tabs for this extension
+    stdout.println("Hello output");
+    stderr.println("Hello errors");
+
+    // Write a message to the Burp alerts tab
+    callbacks.issueAlert("Hello alerts");
+
+    // Throw an exception that will appear in the error tab for this extension
+    throw RuntimeException("Hello exception");
+}
